db05149fbf68a00ad4a22e9d4ade906635849d1c
marsplus <sixie.yu@wustl.edu>
2021-06-23 11:02:10 -0700

    deleted exp.sh

--- exp.sh
+++
@@ -1,7 +0,0 @@
-python src/main.py BA 1 1
-#python src/main.py Small-World 1 50
-#python src/main.py BA 2 50
-#python src/main.py Small-World 2 50
-#python src/main.py BA 3 50
-#python src/main.py Small-World 3 50
-#python src/main.py Facebook 1 50
